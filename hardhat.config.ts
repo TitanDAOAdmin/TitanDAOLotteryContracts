33bfe3562779e8e12737c8d0275cd606d0496ad3
TitanDAOAdmin <86993382+TitanDAOAdmin@users.noreply.github.com>
2021-07-25 00:35:53 +1000

    Contract updats

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,33 +23,16 @@ const config: HardhatUserConfig = {
     hardhat: {
       accounts: accounts('localhost'),
     },
-    localhost: {
-      url: 'http://localhost:8545',
-      accounts: accounts('localhost'),
-    },
-    testnet: {
-      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-      accounts: accounts('testnet'),
-      live: true,
-    },
-    mainnet: {
-      url: 'https://bsc-dataseed.binance.org',
-      accounts: accounts('mainnet'),
-      live: true,
-    },
     matic: {
       url: 'https://rpc-mainnet.maticvigil.com/v1/a50eb5139e4cb2ce865cf47c1b664985eb69b86e',
       accounts: accounts('matic'),
       live: true,
     },
   },
-  gasReporter: {
-    currency: 'USD',
-    gasPrice: 5,
-    enabled: !!process.env.REPORT_GAS,
-  },
   namedAccounts: {
+    test: 0,
     creator: 1,
+    deployer: 1,
   },
 };
 
